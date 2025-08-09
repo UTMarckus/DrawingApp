194b78fe8bfef363be8f177b4f743a6ceeab4ba6
T. Ustiugov <utmarckus@gmail.com>
2025-08-09 14:05:38 +0300

    feat: finish change brush size

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -1,18 +1,53 @@
 package com.example.drawingapp
 
+import android.app.Dialog
 import android.os.Bundle
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.drawingapp.databinding.ActivityMainBinding
+import com.example.drawingapp.databinding.DialogBrushBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var bindingMainActivity: ActivityMainBinding
+    private lateinit var bindingBrushDialog: DialogBrushBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        bindingMainActivity = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingMainActivity.root)
 
-        binding.drawingView.changeBrushSize(24F)
+        bindingBrushDialog = DialogBrushBinding.inflate(layoutInflater)
+
+        bindingMainActivity.drawingView.changeBrushSize(23F)
+
+        bindingMainActivity.brushButton.setOnClickListener { showBrushChooserDialog() }
+    }
+
+    private fun showBrushChooserDialog() {
+        val brushDialog = Dialog(this@MainActivity).apply { setContentView(bindingBrushDialog.root) }
+        val seekBarProgress = bindingBrushDialog.dialogSeekBar
+        val showProgressTV = bindingBrushDialog.dialogTvProgress
+
+        seekBarProgress.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                seekBar: SeekBar,
+                progress: Int,
+                fromUser: Boolean
+            ) {
+                bindingMainActivity.drawingView.changeBrushSize(seekBar.progress.toFloat())
+                showProgressTV.text = seekBar.progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
+
+        brushDialog.show()
     }
 }
