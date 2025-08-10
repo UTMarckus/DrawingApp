a797c57db4d8f44b898108302396311a2a6c4849
T. Ustiugov <utmarckus@gmail.com>
2025-08-10 15:59:30 +0300

    feat: add permissions

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.drawingapp
 
+import android.Manifest
 import android.app.Dialog
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.SeekBar
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.graphics.createBitmap
 import com.example.drawingapp.databinding.ActivityMainBinding
 import com.example.drawingapp.databinding.DialogBrushBinding
 import yuku.ambilwarna.AmbilWarnaDialog
@@ -14,6 +22,19 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var bindingMainActivity: ActivityMainBinding
     private lateinit var bindingBrushDialog: DialogBrushBinding
+
+    val requestPermission: ActivityResultLauncher<Array<String>> = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        permissions.entries.forEach {
+            val (permissionName, isGranted) = it
+
+            if (permissionName == android.Manifest.permission.READ_EXTERNAL_STORAGE) {
+                if (isGranted) Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show()
+                else Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +57,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         bindingMainActivity.undoButton.setOnClickListener(this)
         bindingMainActivity.colorPickerButton.setOnClickListener(this)
+        bindingMainActivity.galleryButton.setOnClickListener(this)
 
     }
 
@@ -79,21 +101,53 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
             R.id.undo_button -> bindingMainActivity.drawingView.undoPath()
             R.id.color_picker_button -> showColorPickerDialog()
+            R.id.gallery_button -> {
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    requestStoragePermission()
+                } else {
+                    // dsa
+                }
+            }
         }
     }
 
     private fun showColorPickerDialog() {
-        val dialog = AmbilWarnaDialog(this, Color.GREEN, object: AmbilWarnaDialog.OnAmbilWarnaListener {
-                    override fun onCancel(dialog: AmbilWarnaDialog?) { }
+        val dialog =
+            AmbilWarnaDialog(this, Color.GREEN, object : AmbilWarnaDialog.OnAmbilWarnaListener {
+                override fun onCancel(dialog: AmbilWarnaDialog?) {}
 
-                    override fun onOk(
-                        dialog: AmbilWarnaDialog?,
-                        color: Int
-                    ) {
-                        bindingMainActivity.drawingView.setBrushColor(color)
-                    }
-                })
+                override fun onOk(
+                    dialog: AmbilWarnaDialog?,
+                    color: Int
+                ) {
+                    bindingMainActivity.drawingView.setBrushColor(color)
+                }
+            })
 
         dialog.show()
+    }
+
+    private fun requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        ) {
+            showRationalDialog()
+        } else {
+            requestPermission.launch(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE))
+        }
+    }
+
+    private fun showRationalDialog() = AlertDialog.Builder(this).apply {
+        setTitle("Storage permission")
+        setMessage("We need this permission in order to access the internal storage")
+        setPositiveButton(R.string.dialog_yes) { dialog, _ -> dialog.dismiss() }
+        create()
+        show()
     }
 }
